18ecccf766a5ab9fe5acf2278036eeb82692331a
Kimberley Le Bherz <kimberley_lebherz@yahoo.com>
2022-06-24 14:10:15 +1000

    added regex for star sign icons

--- script.js
+++ script.js
@@ -86,31 +86,31 @@ age.addEventListener('blur', (e) =>{
                     console.log ("matched leo");
                     return 'Starsign_Leo_NH_Icon.png'
 
-                } else if (age.value.match()){ //virgo Aug 23 - Sep 22
+                } else if (age.value.match(/^[0-9]{4}-0((8-(2[3-9]|3[0-1]))|(9-(0[1-9]|1[0-9]|2[0-2])))$/g)){ //virgo Aug 23 - Sep 22
                     console.log ("matched virgo");
                     return 'Starsign_Virgo_NH_Icon.png'
 
-                } else if (age.value.match()){ //libra Sep 23 - Oct 23
+                } else if (age.value.match(/^[0-9]{4}-((09-(2[3-9]|30))|(10-(0[1-9]|1[0-9]|2[0-3])))$/g)){ //libra Sep 23 - Oct 23
                     console.log ("matched libra");
                     return 'Starsign_Libra_NH_Icon.png'
 
-                } else if (age.value.match()){ //scorpio Oct 24 - Nov 22
+                } else if (age.value.match(/^[0-9]{4}-((10-(2[4-9]|3[0-1]))|(11-(0[1-9]|1[0-9]|2[0-2])))$/g)){ //scorpio Oct 24 - Nov 22
                     console.log ("matched scorpio");
                     return 'Starsign_Scorpio_NH_Icon.png'
 
-                } else if (age.value.match()){ //sagittarius Nov 23 - Dec 21
+                } else if (age.value.match(/^[0-9]{4}-((11-(2[3-9]|30))|(12-(0[1-9]|1[0-9]|2[0-1])))$/g)){ //sagittarius Nov 23 - Dec 21
                     console.log ("matched sagittarius");
                     return 'Starsign_Sagittarius_NH_Icon.png'
                 }
-                else if (age.value.match()){ //capricorn Dec 22 - Jan 19
+                else if (age.value.match(/^[0-9]{4}-((12-(2[2-9]|3[0-1]))|(01-(0[1-9]|1[0-9])))$/g)){ //capricorn Dec 22 - Jan 19
                     console.log ("matched capricorn");
                     return 'Starsign_Capricorn_NH_Icon.png'
                 }
-                else if (age.value.match()){ //aquarius Jan 20 - Feb 18
+                else if (age.value.match(/^[0-9]{4}-0((1-(2[0-9]|30))|(2-(0[1-9]|1[0-8])))$/g)){ //aquarius Jan 20 - Feb 18
                     console.log ("matched aquarius");
                     return 'Starsign_Aquarius_NH_Icon.png'
                 }
-                else if (age.value.match()){ //pisces Feb 19 - Mar 20
+                else if (age.value.match(/^[0-9]{4}-0((2-(19|2[0-9]))|(3-(0[1-9]|1[0-9]|20)))$/g)){ //pisces Feb 19 - Mar 20
                     console.log ("matched pisces");
                     return 'Starsign_Pisces_NH_Icon.png'
                 }
